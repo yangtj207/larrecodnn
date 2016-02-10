d3f86958dcdd899f07d870d241e259c3babc599a
Lynn Garren <garren@fnal.gov>
2016-02-10 09:37:14 -0600

    move RecoAlg to larreco/RecoAlg

--- RecoAlg/CMTool/CMToolBase/CBoolAlgoBase.cxx
+++
@@ -1,6 +0,0 @@
-#ifndef RECOTOOL_CBOOLALGOBASE_CXX
-#define RECOTOOL_CBOOLALGOBASE_CXX
-
-#include "CBoolAlgoBase.h"
-
-#endif
--- RecoAlg/CMTool/CMToolBase/CMAlgoBase.cxx
+++
@@ -1,6 +0,0 @@
-#ifndef RECOTOOL_CMALGOBASE_CXX
-#define RECOTOOL_CMALGOBASE_CXX
-
-#include "CMAlgoBase.h"
-
-#endif
