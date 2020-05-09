efb28b4cbb4dbb0fb778638668729d733e7849bc
Tingjun Yang <tjyang@fnal.gov>
2020-05-08 21:44:13 -0500

    Add support for rawdigits.

--- larrecodnn/ImagePatternAlgs/Tensorflow/Modules/WaveformRoiFinder_module.cc
+++ larrecodnn/ImagePatternAlgs/Tensorflow/Modules/WaveformRoiFinder_module.cc
@@ -25,8 +25,10 @@
 #include "canvas/Utilities/InputTag.h"
 
 #include "lardataobj/RecoBase/Wire.h"
+#include "lardataobj/RawData/RawDigit.h"
+#include "lardataobj/RawData/raw.h"
+#include "larcore/Geometry/Geometry.h"
 #include "larrecodnn/ImagePatternAlgs/Tensorflow/WaveformRecogTools/IWaveformRecog.h"
-
 
 #include <memory>
 
@@ -66,6 +68,7 @@ private:
   unsigned int fNumStrides;
   unsigned int fLastWindowSize;
 
+  art::InputTag fRawProducerLabel;
   art::InputTag fWireProducerLabel;
   std::unique_ptr<wavrec_tool::IWaveformRecog> fWaveformRecogTool;
 
@@ -79,8 +82,17 @@ nnet::WaveformRoiFinder::WaveformRoiFinder(fhicl::ParameterSet const& p)
   fWaveformSize(p.get<unsigned int>("WaveformSize", 6000)),
   fWindowSize(p.get<unsigned int>("ScanWindowSize", 200)),
   fStrideLength(p.get<unsigned int>("StrideLength", 150)),
-  fWireProducerLabel(p.get<art::InputTag>("WireProducerLabel"))
+  fRawProducerLabel(p.get<art::InputTag>("RawProducerLabel","")),
+  fWireProducerLabel(p.get<art::InputTag>("WireProducerLabel",""))
 {
+
+  if (fRawProducerLabel.empty() && fWireProducerLabel.empty()){
+    throw cet::exception("WaveformRoiFinder") << "Both RawProducerLabel and WireProducerLabel are empty";
+  }
+
+  if ((!fRawProducerLabel.empty()) && (!fWireProducerLabel.empty())){
+    throw cet::exception("WaveformRoiFinder") << "Only one of RawProducerLabel and WireProducerLabel should be set";
+  }
 
   cet::search_path sp("FW_SEARCH_PATH");
   if( !sp.find_file(fMeanFilename, fMeanPathname) ) {
@@ -133,23 +145,63 @@ nnet::WaveformRoiFinder::WaveformRoiFinder(fhicl::ParameterSet const& p)
 void nnet::WaveformRoiFinder::produce(art::Event& e)
 {
 
-  auto wireHandle = e.getValidHandle< std::vector<recob::Wire> >(fWireProducerLabel);
+  std::cout<<fRawProducerLabel<<std::endl;
+
+  art::Handle< std::vector<raw::RawDigit> > rawListHandle;
+  std::vector<art::Ptr<raw::RawDigit> > rawlist;
+  if (e.getByLabel(fRawProducerLabel, rawListHandle))
+    art::fill_ptr_vector(rawlist, rawListHandle);
+
+  //std::cout<<rawlist.size()<<std::endl;
+
+  art::Handle< std::vector<recob::Wire> > wireListHandle;
+  std::vector<art::Ptr<recob::Wire> > wirelist;
+  if (e.getByLabel(fWireProducerLabel, wireListHandle))
+    art::fill_ptr_vector(wirelist, wireListHandle);
+
+  //auto wireHandle = e.getValidHandle< std::vector<recob::Wire> >(fWireProducerLabel);
 
   std::unique_ptr<std::vector<recob::Wire> > outwires(new std::vector<recob::Wire>);
+
+  auto const* geo = lar::providerFrom<geo::Geometry>();
 
   //##############################
   //### Looping over the wires ###
   //##############################
-  for(size_t wireIter = 0; wireIter < wireHandle->size(); wireIter++){
-
-    art::Ptr<recob::Wire>   wire(wireHandle, wireIter);
-    const auto & signal = wire->Signal();
+  //for(size_t wireIter = 0; wireIter < wireHandle->size(); wireIter++){
+  //std::cout<<"size = "<<(rawlist.empty()?wirelist.size():rawlist.size())<<std::endl;
+  for (unsigned int ich = 0; ich < (rawlist.empty()?wirelist.size():rawlist.size()); ++ich){
+    
+    std::vector<float> inputsignal(fWaveformSize);
     std::vector<float> adc(fWaveformSize);
 
-    for (size_t itck = 0; itck < adc.size(); ++itck){
-      adc[itck] = (signal[itck] - meanvec[itck])/scalevec[itck];
-    }
+    if (!wirelist.empty()){
+      const auto & wire = wirelist[ich];
+      //art::Ptr<recob::Wire>   wire(wireHandle, wireIter);
+      const auto & signal = wire->Signal();
 
+      for (size_t itck = 0; itck < adc.size(); ++itck){
+        inputsignal[itck] = signal[itck];
+        adc[itck] = (inputsignal[itck] - meanvec[itck])/scalevec[itck];
+      }
+    }
+    else if (!rawlist.empty()){
+      //std::cout<<ich<<std::endl;
+      const auto & digitVec = rawlist[ich];
+      std::vector<short> rawadc(fWaveformSize);
+      raw::Uncompress(digitVec->ADCs(), rawadc, digitVec->GetPedestal(), digitVec->Compression());
+      for (size_t itck = 0; itck < rawadc.size(); ++itck){
+        inputsignal[itck] = rawadc[itck] - digitVec->GetPedestal();
+        //std::cout<<rawadc[itck]<<" "<<digitVec->GetPedestal()<<std::endl;
+        if (geo->View(rawlist[ich]->Channel()) == 2){
+          inputsignal[itck] -= 900;
+        }
+        else{
+          inputsignal[itck] -= 2350;
+        }
+        adc[itck] = (inputsignal[itck] - meanvec[itck])/scalevec[itck];
+      }
+    }
     // .. create a vector of windows
     std::vector<std::vector<float>>wwv(fNumStrides+1, std::vector<float>(fWindowSize,0.));
     std::vector<std::vector<float>>predv(fNumStrides+1, std::vector<float>(1,0.));
@@ -184,7 +236,7 @@ void nnet::WaveformRoiFinder::produce(art::Event& e)
     recob::Wire::RegionsOfInterest_t rois(fWaveformSize);
 
     //for (size_t i = 0; i<predv.size(); ++i){
-    for (size_t i = 0; i<signal.size(); ++i){
+    for (size_t i = 0; i<fWaveformSize; ++i){
       bool isroi = false;
       // For ProtoDUNE, shape of predv is (40,1) for 40 windows and a single output/categor.
       // The window size is 200 ticks. The algorithm scans across the entire 6000 tick waveform in strides or step sizes of 150 ticks
@@ -199,7 +251,7 @@ void nnet::WaveformRoiFinder::produce(art::Event& e)
       }
       if (isroi){
         if (sigs.empty()){
-          sigs.push_back(signal[i]);
+          sigs.push_back(inputsignal[i]);
           lastsignaltick = i;
           roistart = i;
         }
@@ -207,12 +259,12 @@ void nnet::WaveformRoiFinder::produce(art::Event& e)
           if (int(i)!=lastsignaltick+1){
             rois.add_range(roistart, std::move(sigs));
             sigs.clear();
-            sigs.push_back(signal[i]);
+            sigs.push_back(inputsignal[i]);
             lastsignaltick = i;
             roistart = i;
           }
           else {
-            sigs.push_back(signal[i]);
+            sigs.push_back(inputsignal[i]);
             lastsignaltick = i;
           }
         }
@@ -221,7 +273,12 @@ void nnet::WaveformRoiFinder::produce(art::Event& e)
     if (!sigs.empty()){
       rois.add_range(roistart, std::move(sigs));
     }
-    outwires->emplace_back(recob::Wire(rois, wire->Channel(), wire->View()));
+    if (!wirelist.empty()){
+      outwires->emplace_back(recob::Wire(rois, wirelist[ich]->Channel(), wirelist[ich]->View()));
+    }
+    else if (!rawlist.empty()){
+      outwires->emplace_back(recob::Wire(rois, rawlist[ich]->Channel(), geo->View(rawlist[ich]->Channel())));
+    }
   }
 
   e.put(std::move(outwires));
